eec0b3a6fecc4cffd7741bd8bde293f3a897165d
shai-D2K <nigelgirouard@outlook.com>
2018-11-29 14:46:54 -0500

    Created Library and added some empty table models

---
+++ EmmaLibrary/Manufacturer.cs
@@ -0,0 +1,23 @@
+﻿/*
+
+    Created by:         Nigel Girouard
+    Date:               2018-11-29
+    Last modified by:   Nigel Girouard
+    Date:               2018-11-29
+    Description:        Manufacturer table
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmmaLibrary
+{
+    public class Manufacturer
+    {
+
+    }
+}
---
+++ EmmaLibrary/Service_Order.cs
@@ -0,0 +1,23 @@
+﻿/*
+
+    Created by:         Nigel Girouard
+    Date:               2018-11-29
+    Last modified by:   Nigel Girouard
+    Date:               2018-11-29
+    Description:        Service_Order table
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmmaLibrary
+{
+    public class Service_Order
+    {
+
+    }
+}
