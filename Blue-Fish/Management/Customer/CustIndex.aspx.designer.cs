a7e378c0ce3bc05e0ad189e3237cb3274b9a0265
Will Ashcroft <washcroft2@ncstudents.niagaracollege.ca>
2018-11-29 22:44:44 -0500

    Pulling records for customer index and constructing table, search not implemented

--- Blue-Fish/Management/Customer/CustIndex.aspx.designer.cs
+++ Blue-Fish/Management/Customer/CustIndex.aspx.designer.cs
@@ -38,5 +38,14 @@ namespace Blue_Fish {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox txtSearchPhoneNumber;
+        
+        /// <summary>
+        /// tblCustomer control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Table tblCustomer;
     }
 }
