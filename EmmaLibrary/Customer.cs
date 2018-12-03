630dd4f6dd8808931d92150ef11eca4f47792e3c
Nigel <nigelgirouard@outlook.com>
2018-12-03 15:20:52 -0500

    Added C/U/D code for customer, Open() and Close() methods for DataConnection

--- EmmaLibrary/Customer.cs
+++ EmmaLibrary/Customer.cs
@@ -38,5 +38,102 @@ namespace EmmaLibrary
             this.custPostal = custPostal;
             this.custEmail = custEmail;
         }
+        
+        public static bool CreateCustomer(Customer customer, out string status)
+        {
+            status = DataConnection.status;
+
+            DataConnection.command.CommandText =
+                "INSERT INTO Customer(custFirst, custLast, custPhone, custAddress, " +
+                "custCity, custPostal, custEmail) " +
+                "VALUES(" + 
+                "'" + customer.custFirst + "', " +
+                "'" + customer.custLast + "', " +
+                "'" + customer.custPhone + "', " +
+                "'" + customer.custAddress + "', " +
+                "'" + customer.custCity + "', " +
+                "'" + customer.custPostal + "', " +
+                "'" + customer.custEmail + "', " +
+                ")";
+
+            if (DataConnection.Open())
+            {
+                try
+                {
+                    DataConnection.command.ExecuteNonQuery();
+                    status = "Insert successful";
+                    DataConnection.Close();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    status = "Insert failed\n" + e.Message;
+
+                }
+            }
+
+            DataConnection.Close();
+            return false;
+        }
+
+        public static bool UpdateCustomer(Customer customer, out string status)
+        {
+            status = DataConnection.status;
+
+            DataConnection.command.CommandText = 
+                "UPDATE Customer SET custFirst '" + customer.custFirst + "', " + 
+                "custLast = '" + customer.custLast + "', " +
+                "custPhone = '" + customer.custPhone + "', " +
+                "custAddress = '" + customer.custAddress + "', " +
+                "custCity = '" + customer.custCity + "', " +
+                "custPostal = '" + customer.custPostal + "', " +
+                "custEmail = '" + customer.custEmail +  "' " + 
+                "WHERE ID = '" + customer.id.ToString();
+
+            if(DataConnection.Open())
+            {
+                try
+                {
+                    DataConnection.command.ExecuteNonQuery();
+                    status = "Update successful";
+                    DataConnection.Close();
+                    return true;
+                }
+                catch(Exception e)
+                {
+                    status = "Update failed\n" + e.Message;
+                    
+                }
+            }
+
+            DataConnection.Close();
+            return false;
+        }
+
+        public static bool DeleteCustomer(Customer customer, out string status)
+        {
+            status = DataConnection.status;
+
+            DataConnection.command.CommandText = "DELETE FROM Customer WHERE id = " + customer.id.ToString();
+
+            if (DataConnection.Open())
+            {
+                try
+                {
+                    DataConnection.command.ExecuteNonQuery();
+                    status = "Delete successful";
+                    DataConnection.Close();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    status = "Delete failed\n" + e.Message;
+
+                }
+            }
+
+            DataConnection.Close();
+            return false;
+        }
     }
 }
--- EmmaLibrary/DataConnection.cs
+++ EmmaLibrary/DataConnection.cs
@@ -4,7 +4,7 @@
     Date:               2018-12-03
     Last modified by:   Nigel Girouard
     Date:               2018-12-03
-    Description:        Use this class to connect to the database
+    Description:        Use this class to connect to the database from within the library 
 
 */
 
@@ -18,8 +18,57 @@ using System.Threading.Tasks;
 
 namespace EmmaLibrary
 {
+    /// <summary>
+    /// Use this class to interact with the database inside the Library
+    /// </summary>
     internal static class DataConnection
     {
+        private static SqlConnection connection = new SqlConnection();
         internal static SqlCommand command = new SqlCommand();
+        internal static string status = "Ready";
+
+        static DataConnection()
+        {
+            connection.ConnectionString = global::EmmaLibrary.Properties.Settings.Default.EmmasLibraryConnectionString;
+            command.Connection = connection;
+        }
+
+        /// <summary>
+        /// Open a connection with the database. Returns true if successful, false if an exception occures.
+        /// </summary>
+        /// <returns>Returns true if successful, false if an exception occures</returns>
+        internal static bool Open()
+        {
+            status = "Ready";
+
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch(Exception e)
+            {
+                status = "Database failed to open\n" + e.Message;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Close an opened connection with the database.
+        /// </summary>
+        internal static void Close()
+        {
+            try
+            {
+                if(connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+            catch(Exception e)
+            {
+                status = "Database failed to close\n" + e.Message;
+            }
+        }
     }
 }
