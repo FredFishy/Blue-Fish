830a392419e989ae91ef7404638d4ad4c0b1a43d
PaulVD <pvandyk1@ncstudents.niagaracollege.ca>
2018-12-03 13:53:28 -0500

    Made classes receipt, order_line, inventory, product, payment for library

---
+++ EmmaLibrary/Inventory.cs
@@ -0,0 +1,33 @@
+﻿//Paul Vandyk
+//Created 12/03/2018
+//Edited 12/03/2018
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmmaLibrary
+{
+    class Inventory
+    {
+        public readonly int id;
+        public int invQuantity;
+        public decimal invSize;
+        public string invMeasure;
+        public decimal invPrice;
+        public int productID;
+
+        public Inventory(int id, int invQuantity, decimal invSize, string invMeasure, decimal invPrice, int productID)
+        {
+            this.id = id;
+            this.invQuantity = invQuantity;
+            this.invSize = invSize;
+            this.invMeasure = invMeasure;
+            this.invPrice = invPrice;
+            this.productID = productID;
+        }
+    }
+}
---
+++ EmmaLibrary/Order_Line.cs
@@ -0,0 +1,35 @@
+﻿//Paul Vandyk
+//Created 12/03/2018
+//Edited 12/03/2018
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmmaLibrary
+{
+    class Order_Line
+    {
+        public readonly int id;
+        public decimal orlPrice;
+        public int orlQuantity;
+        public bool orlOrderReq;
+        public string orlNote;
+        public int inventoryID;
+        public int receiptID;
+
+        public Order_Line(int id, decimal orlPrice, int orlQuantity, bool orlOrderReq, string orlNote, int inventoryID, int receiptID)
+        {
+            this.id = id;
+            this.orlPrice = orlPrice;
+            this.orlQuantity = orlQuantity;
+            this.orlOrderReq = orlOrderReq;
+            this.orlNote = orlNote;
+            this.inventoryID = inventoryID;
+            this.receiptID = receiptID;
+        }
+    }
+}
