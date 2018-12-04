0179777ee68df6d004b162b95e937e3493645fec
Paul Vandyk <pvandyk1@ncstudents.niagaracollege.ca>
2018-12-04 11:14:38 -0500

    Finished CRUD on my pieces of the Library

--- EmmaLibrary/Order_Line.cs
+++ EmmaLibrary/Order_Line.cs
@@ -1,10 +1,13 @@
 ﻿//Paul Vandyk
 //Created 12/03/2018
+
+//Paul Vandyk
 //Edited 12/03/2018
 
 
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,15 +24,144 @@ namespace EmmaLibrary
         public int inventoryID;
         public int receiptID;
 
-        public Order_Line(int id, decimal orlPrice, int orlQuantity, bool orlOrderReq, string orlNote, int inventoryID, int receiptID)
+        public Order_Line()
+        {
+            this.id = -1;
+        }
+        public Order_Line(int id)
         {
             this.id = id;
-            this.orlPrice = orlPrice;
-            this.orlQuantity = orlQuantity;
-            this.orlOrderReq = orlOrderReq;
-            this.orlNote = orlNote;
-            this.inventoryID = inventoryID;
-            this.receiptID = receiptID;
+        }
+
+        public static List<Order_Line> GetAllOrder_Lines(out string status)
+        {
+            status = DataConnection.status;
+            List<Order_Line> order_Lines = new List<Order_Line>();
+            DataConnection.command.CommandText = "Select * From Movie";
+            if (DataConnection.Open())
+            {
+                try
+                {
+                    SqlDataReader reader = DataConnection.command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Order_Line orderLine = new Order_Line(Convert.ToInt32(reader[0]));
+                        orderLine.orlPrice = Convert.ToDecimal(reader[1]);
+                        orderLine.orlQuantity = Convert.ToInt32(reader[2]);
+                        orderLine.orlOrderReq = Convert.ToBoolean(reader[3]);
+                        orderLine.orlNote = reader[4].ToString();
+                        orderLine.inventoryID = Convert.ToInt32(reader[5]);
+                        orderLine.receiptID = Convert.ToInt32(reader[6]);
+                        order_Lines.Add(orderLine);
+                    }
+                    status = "Records Found";
+                }
+                catch (Exception ex)
+                {
+                    status = "Command Failed\n" + ex.Message;
+                }
+                finally
+                {
+                    DataConnection.Close();
+                }
+            }
+            return order_Lines;
+        }
+
+        public static bool CreateOrder_Line(Order_Line order_Line, out string status, out int id)
+        {
+            status = DataConnection.status;
+
+            DataConnection.command.CommandText =
+                "INSERT INTO Order_Line(orlPrice, orlQuantity, orlOrderReq, orlNote, inventoryID, receiptID) " +
+                "OUTPUT INSERTED.ID " +
+                "VALUES(" +
+                "'" + order_Line.orlPrice + "', " +
+                "'" + order_Line.orlQuantity + "', " +
+                "'" + order_Line.orlOrderReq + "', " +
+                "'" + order_Line.orlNote + "', " +
+                "'" + order_Line.inventoryID + "', " +
+                "'" + order_Line.receiptID + "', " +
+                ")";
+
+            if (DataConnection.Open())
+            {
+                try
+                {
+                    id = (int)DataConnection.command.ExecuteScalar();
+                    status = "Insert successful";
+                    DataConnection.Close();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    status = "Insert failed\n" + e.Message;
+
+                }
+            }
+
+            DataConnection.Close();
+            id = 0;
+            return false;
+        }
+
+        public static bool UpdateOrder_Line(Order_Line order_Line, out string status)
+        {
+            status = DataConnection.status;
+
+            DataConnection.command.CommandText =
+                "UPDATE Order_Line SET orlPrice '" + order_Line.orlPrice + "', " +
+                "orlQuantity = '" + order_Line.orlQuantity + "', " +
+                "orlOrderReq = '" + order_Line.orlOrderReq + "', " +
+                "orlNote = '" + order_Line.orlNote + "', " +
+                "inventoryID = '" + order_Line.inventoryID + "', " +
+                "receiptID = '" + order_Line.receiptID + "' " +
+                "WHERE ID = '" + order_Line.id.ToString();
+
+            if (DataConnection.Open())
+            {
+                try
+                {
+                    DataConnection.command.ExecuteNonQuery();
+                    status = "Update successful";
+                    DataConnection.Close();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    status = "Update failed\n" + e.Message;
+
+                }
+            }
+
+            DataConnection.Close();
+            return false;
+        }
+
+        public static bool Order_LineDelete(Order_Line order_Line, out string status)
+        {
+            status = DataConnection.status;
+
+            DataConnection.command.CommandText = "DELETE FROM Order_Line WHERE id = " + order_Line.id.ToString();
+
+            if (DataConnection.Open())
+            {
+                try
+                {
+                    DataConnection.command.ExecuteNonQuery();
+                    status = "Delete successful";
+                    DataConnection.Close();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    status = "Delete failed\n" + e.Message;
+
+                }
+            }
+
+            DataConnection.Close();
+            return false;
         }
     }
 }
--- EmmaLibrary/Receipt.cs
+++ EmmaLibrary/Receipt.cs
@@ -1,35 +1,169 @@
 ﻿//Paul Vandyk
 //Created 12/03/2018
+
+//Paul Vandyk
 //Edited 12/03/2018
 
 
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace EmmaLibrary
 {
     public class Receipt
     {
         public readonly int id;
-        public string ordNumber;
-        public DateTime ordDate;
-        public bool ordPaid;
-        public int paymentID;
-        public int custID;
-        public int empID;
+        public string ordNumber = "Unknown";
+        public DateTime ordDate = new DateTime(0001,01,01);
+        public bool ordPaid = false;
+        public int paymentID = -1;
+        public int custID = -1;
+        public int empID = -1;
 
-        public Receipt(int id, string ordNumber, DateTime ordDate, bool ordPaid, int paymentID, int custID, int empID)
+        public Receipt()
+        {
+            this.id = -1;
+        }
+        private Receipt(int id)
         {
             this.id = id;
-            this.ordNumber = ordNumber;
-            this.ordDate = ordDate;
-            this.ordPaid = ordPaid;
-            this.paymentID = paymentID;
-            this.custID = custID;
-            this.empID = empID;
+        }
+
+        public static List<Receipt> GetAllReceipts(out string status)
+        {
+            status = DataConnection.status;
+            List<Receipt> receipts = new List<Receipt>();
+            DataConnection.command.CommandText = "Select * From Movie";
+            if (DataConnection.Open())
+            {
+                try
+                {
+                    SqlDataReader reader = DataConnection.command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Receipt receipt = new Receipt(Convert.ToInt32(reader[0]));
+                        receipt.ordNumber = reader[1].ToString();
+                        receipt.ordDate = Convert.ToDateTime(reader[2]);
+                        receipt.ordPaid = Convert.ToBoolean(reader[3]);
+                        receipt.paymentID = Convert.ToInt32(reader[4]);
+                        receipt.custID = Convert.ToInt32(reader[5]);
+                        receipt.empID = Convert.ToInt32(reader[6]);
+                        receipts.Add(receipt);
+                    }
+                    status = "Records Found";
+                }
+                catch (Exception ex)
+                {
+                    status = "Command Failed\n" + ex.Message;
+                }
+                finally
+                {
+                    DataConnection.Close();
+                }
+            }
+            return receipts;
+        }
+
+        public static bool CreateReceipt(Receipt receipt, out string status, out int id)
+        {
+            status = DataConnection.status;
+
+            DataConnection.command.CommandText =
+                "INSERT INTO Receipt(ordNumber, ordDate, ordPaid, paymentID, " +
+                "custID, empID) " +
+                "OUTPUT INSERTED.ID " +
+                "VALUES(" +
+                "'" + receipt.ordNumber + "', " +
+                "'" + receipt.ordDate + "', " +
+                "'" + receipt.ordPaid + "', " +
+                "'" + receipt.paymentID + "', " +
+                "'" + receipt.custID + "', " +
+                "'" + receipt.empID + "' " +
+                ")";
+
+            if (DataConnection.Open())
+            {
+                try
+                {
+                    id = (int)DataConnection.command.ExecuteScalar();
+                    status = "Insert successful";
+                    DataConnection.Close();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    status = "Insert failed\n" + e.Message;
+
+                }
+            }
+
+            DataConnection.Close();
+            id = 0;
+            return false;
+        }
+
+        public static bool UpdateReceipt(Receipt receipt, out string status)
+        {
+            status = DataConnection.status;
+
+            DataConnection.command.CommandText =
+                "UPDATE Receipt SET ordNumber '" + receipt.ordNumber + "', " +
+                "ordDate = '" + receipt.ordDate + "', " +
+                "ordPaid = '" + receipt.ordPaid + "', " +
+                "paymentID = '" + receipt.paymentID + "', " +
+                "custID = '" + receipt.custID + "', " +
+                "empID = '" + receipt.empID + "' " +
+                "WHERE ID = '" + receipt.id.ToString();
+
+            if (DataConnection.Open())
+            {
+                try
+                {
+                    DataConnection.command.ExecuteNonQuery();
+                    status = "Update successful";
+                    DataConnection.Close();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    status = "Update failed\n" + e.Message;
+
+                }
+            }
+
+            DataConnection.Close();
+            return false;
+        }
+
+        public static bool DeleteReceipt(Receipt receipt, out string status)
+        {
+            status = DataConnection.status;
+
+            DataConnection.command.CommandText = "DELETE FROM Customer WHERE id = " + receipt.id.ToString();
+
+            if (DataConnection.Open())
+            {
+                try
+                {
+                    DataConnection.command.ExecuteNonQuery();
+                    status = "Delete successful";
+                    DataConnection.Close();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    status = "Delete failed\n" + e.Message;
+
+                }
+            }
+
+            DataConnection.Close();
+            return false;
         }
     }
 }
