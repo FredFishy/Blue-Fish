2fd9694c9211a1fe818e2650438c5d8895143512
Nigel <nigelgirouard@outlook.com>
2018-12-13 18:32:26 -0500

    Undid navbar hiding

--- Blue-Fish/Site.Master.cs
+++ Blue-Fish/Site.Master.cs
@@ -22,65 +22,65 @@ namespace Blue_Fish
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!HttpContext.Current.User.Identity.IsAuthenticated)
-            {
-                registerLink.Visible = true;
-                linkCustomer.Visible = false;
-                linkSales.Visible = false;
-                linkInventory.Visible = false;
-                linkOrdering.Visible = false;
-                linkRepair.Visible = false;
-                linkDDLManagment.Visible = false;
-            }
-            else
-            {
-                registerLink.Visible = false;
-                if (HttpContext.Current.User.Identity.Name == "emma@ese.com")
-                {
-                    linkCustomer.Visible = true;
-                    linkSales.Visible = true;
-                    linkInventory.Visible = true;
-                    linkOrdering.Visible = true;
-                    linkRepair.Visible = true;
-                    linkDDLManagment.Visible = true;
-                }
-                else if (HttpContext.Current.User.Identity.Name == "ordering@ese.com")
-                {
-                    linkCustomer.Visible = true;
-                    linkSales.Visible = true;
-                    linkInventory.Visible = true;
-                    linkOrdering.Visible = true;
-                    linkRepair.Visible = true;
-                    linkDDLManagment.Visible = false;
-                }
-                else if (HttpContext.Current.User.Identity.Name == "tech@ese.com")
-                {
-                    linkCustomer.Visible = true;
-                    linkSales.Visible = true;
-                    linkInventory.Visible = true;
-                    linkOrdering.Visible = false;
-                    linkRepair.Visible = true;
-                    linkDDLManagment.Visible = false;
-                }
-                else if (HttpContext.Current.User.Identity.Name == "sales@ese.com")
-                {
-                    linkCustomer.Visible = true;
-                    linkSales.Visible = true;
-                    linkInventory.Visible = true;
-                    linkOrdering.Visible = true;
-                    linkRepair.Visible = true;
-                    linkDDLManagment.Visible = false;
-                }
-                else
-                {
-                    linkCustomer.Visible = false;
-                    linkSales.Visible = false;
-                    linkInventory.Visible = false;
-                    linkOrdering.Visible = false;
-                    linkRepair.Visible = false;
-                    linkDDLManagment.Visible = false;
-                }
-            }
+            //if (!HttpContext.Current.User.Identity.IsAuthenticated)
+            //{
+            //    registerLink.Visible = true;
+            //    linkCustomer.Visible = false;
+            //    linkSales.Visible = false;
+            //    linkInventory.Visible = false;
+            //    linkOrdering.Visible = false;
+            //    linkRepair.Visible = false;
+            //    linkDDLManagment.Visible = false;
+            //}
+            //else
+            //{
+            //    registerLink.Visible = false;
+            //    if (HttpContext.Current.User.Identity.Name == "emma@ese.com")
+            //    {
+            //        linkCustomer.Visible = true;
+            //        linkSales.Visible = true;
+            //        linkInventory.Visible = true;
+            //        linkOrdering.Visible = true;
+            //        linkRepair.Visible = true;
+            //        linkDDLManagment.Visible = true;
+            //    }
+            //    else if (HttpContext.Current.User.Identity.Name == "ordering@ese.com")
+            //    {
+            //        linkCustomer.Visible = true;
+            //        linkSales.Visible = true;
+            //        linkInventory.Visible = true;
+            //        linkOrdering.Visible = true;
+            //        linkRepair.Visible = true;
+            //        linkDDLManagment.Visible = false;
+            //    }
+            //    else if (HttpContext.Current.User.Identity.Name == "tech@ese.com")
+            //    {
+            //        linkCustomer.Visible = true;
+            //        linkSales.Visible = true;
+            //        linkInventory.Visible = true;
+            //        linkOrdering.Visible = false;
+            //        linkRepair.Visible = true;
+            //        linkDDLManagment.Visible = false;
+            //    }
+            //    else if (HttpContext.Current.User.Identity.Name == "sales@ese.com")
+            //    {
+            //        linkCustomer.Visible = true;
+            //        linkSales.Visible = true;
+            //        linkInventory.Visible = true;
+            //        linkOrdering.Visible = true;
+            //        linkRepair.Visible = true;
+            //        linkDDLManagment.Visible = false;
+            //    }
+            //    else
+            //    {
+            //        linkCustomer.Visible = false;
+            //        linkSales.Visible = false;
+            //        linkInventory.Visible = false;
+            //        linkOrdering.Visible = false;
+            //        linkRepair.Visible = false;
+            //        linkDDLManagment.Visible = false;
+            //    }
+            //}
         }
 
         protected void loginStatus_LoggingOut(object sender, LoginCancelEventArgs e)
